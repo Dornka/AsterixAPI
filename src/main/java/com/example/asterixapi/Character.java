bce0271e598b989559f8a20a332725d412497b74
FIRST_NAME LAST_NAME <Skiber03@gmail.com>
2023-09-15 11:23:30 +0200

    added Character and AsterixController

---
+++ src/main/java/com/example/asterixapi/Character.java
@@ -0,0 +1,47 @@
+package com.example.asterixapi;
+
+public class Character {
+
+    String id;
+    String name;
+    int age;
+    String profession;
+
+    public Character(String id, String name, int age, String profession) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.profession = profession;
+    }
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getProfession() {
+        return profession;
+    }
+
+    public void setProfession(String profession) {
+        this.profession = profession;
+    }
+}
